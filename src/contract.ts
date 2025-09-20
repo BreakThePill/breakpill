d921c5102e3bbce6f2745b93ea84a0e5d3e9070f
BreakThePill <breakthepill@gmail.com>
2025-09-21 00:43:55 +0200

    Update contract.ts

--- src/contract.ts
+++ src/contract.ts
@@ -1,3 +1,3 @@
-export const CONTRACT_ADDRESS = "0xAe04aC3C17B589ca79ec1c221C725b43f055d9d7"; // <— garde UNE seule valeur
+export const CONTRACT_ADDRESS = "0xbf2CfD0c6b0A96e84ED1Ae5630BE0Fbdd1E2A763";
 export const ARB_HEX = "0xa4b1";
 export const ARB_DEC = 42161n;
